60d9baf0309bd04abefba2d382310c9d3eb2a145
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2021-12-09 14:55:15 -0500

    Update overlay.rb

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -5,6 +5,9 @@ include_controls "redhat-enterprise-linux-7-stig-baseline" do
   control "V-71933" do
     impact 0.0
     desc 'caveat', 'This is Not Applicable for the ACME project since we have an approved risk-based decision on 10/1/2020 in CFACTS allowing reuse of old passwords'
+    describe 'This is Not Applicable for the ACME project since we have an approved risk-based decision on 10/1/2020 in CFACTS allowing reuse of old passwords' do
+      skip 'This is Not Applicable for the ACME project since we have an approved risk-based decision on 10/1/2020 in CFACTS allowing reuse of old passwords'
+    end
   end 
   
   control 'V-71943' do
